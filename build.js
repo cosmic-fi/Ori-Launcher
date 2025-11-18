9d50c5d67cb5bfda2de7247509199dbc3fe7098d
cosmic-fi <killrbid@gmail.com>
2025-11-18 20:58:59 +0300

    #20

--- build.js
+++ build.js
@@ -71,6 +71,7 @@ const buildApp = async () => {
                 copyright: `Copyright © ${new Date().getFullYear()} Cosmic-fi (Cosmic Boucher)`,
                 forceCodeSigning: false,
                 afterSign: null,
+                publish: null,
                 artifactName: '${productName}-${version}-${os}-${arch}.${ext}',
                 extraMetadata: {
                     main: 'appsrc/electron/main.js'
